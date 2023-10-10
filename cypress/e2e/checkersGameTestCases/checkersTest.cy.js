ac308cb56e22416f2129d71b3f4cba74dc4cd07e
muhacakmak <mcakmak@styleseat.com>
2023-10-10 03:30:39 -0400

    updating Checkers game tests

--- cypress/e2e/checkersGameTestCases/checkersTest.cy.js
+++ cypress/e2e/checkersGameTestCases/checkersTest.cy.js
@@ -1,34 +1,50 @@
 //The Checkers Game
 
 describe('Checkers Game', () => {
-  it('Playing checkers game', () => {
+
+  beforeEach(() => {
     // Navigating to the website
     cy.visit('https://www.gamesforthebrain.com/game/checkers/', { timeout: 20000 });
+  });
 
-    cy.wait(10000)
+
+  it('Navigating to Checkers Game', () => {
     // Confirming that the site is up
     cy.title().should('contain', 'Checkers');
+  });
 
-    // Step 3: Make five legal moves as orange
-    for (let i = 0; i < 5; i++) {
 
-        cy.get('[name="space62"]').then(($piece) => {
-        const startPosition = $piece.attr('id');
-        const targetPosition = '#' + (parseInt(startPosition) + 7); // Move one row down
+  it('Making Moves in Checkers Game', () => {
 
-        cy.get(targetPosition).click();
-        cy.contains('Make a move').click();
-      });
+    cy.get('#message').should("contain", "Select an orange piece to move.");
+
+    // Defining the moves
+    const moves = [
+      { start: 62, end: 53 },
+      { start: 53, end: 44 },
+      { start: 42, end: 64 },
+      { start: 51, end: 42 },
+      { start: 40, end: 51 },
+    ];
+
+    // Functioning to make a move
+    const makeMoveFunction = (start, end) => {
+      cy.get(`[name="space${start}"]`).click();
+      cy.get(`[name="space${end}"]`).click();
+      cy.wait(2000); // Wait for the blue player to move
+      cy.contains('Make a move').should('be.visible'); // Confirmation that the player can take the next step
+    };
+
+    // Looping through the moves
+    for (const move of moves) {
+      makeMoveFunction(move.start, move.end);
     }
 
-    // Step 4: Include taking a blue piece (assuming it's a legal move)
-    cy.get('.blue').first().click();
-    cy.contains('Make a move').click();
-
-    // Step 5: Restart the game after five moves
+    //Restarting the game after five moves
     cy.contains('Restart').click();
 
-    // Step 6: Confirm that the restarting had been successful
-    cy.get('.orange.active').should('exist');
+    //Confirming that the restarting had been successful
+    cy.get('#message').should("exist");
+    cy.get('#message').should("contain", "Select an orange piece to move.");
   });
 });
